751a3f2201b7ff659e9ce07cc3bb844190f9dfc1
huqiming513 <476008081@qq.com>
2017-09-03 14:17:52 +0800

    没啥

--- app/src/main/java/layout/BorrowFragment.java
+++ app/src/main/java/layout/BorrowFragment.java
@@ -10,7 +10,7 @@ import android.view.ViewGroup;
 
 import com.example.jin.communitymanagement.R;
 
-
+//fuck
 public class BorrowFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
